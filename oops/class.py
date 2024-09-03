71291ddcc58f632733bc77590bdf4baf9fc67cc1
Ashukla011 <shuklavashu9696@gmail.com>
2024-09-03 22:09:37 +0530

    numpy

---
+++ oops/class.py
@@ -0,0 +1,26 @@
+
+# Classes privide a means of bundling data and functionality together 
+# Classes are blue print of object 
+
+
+'''
+1. the class inheritance mechanism allows multiple base classes
+2. A drived class can overrride any methods of its base class or classes
+3. A method can call the method of base class 
+4. 
+'''
+#Syntax :
+'''
+class ClassNmae:
+<statement-1>
+.
+.
+.
+<Statemetn-N>
+'''
+
+# INHERITANCE
+'''
+
+'''
+
